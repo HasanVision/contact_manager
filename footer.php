cb38e6e4b442d6010369afa5f320287a7f698205
HasanVision <hasan.visionx@gmail.com>
2024-09-17 11:06:41 -0400

    initial commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <h2>
+        &copy; 2024 all rights reserved
+    </h2>
+</footer>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require('database.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="css/app.css">
+    <title>Contact Manager - Home</title>
+</head>
+<body>
+    <?php
+    include ("header.php"); 
+    ?>
+    <main>
+<div>
+    <h1>
+        Welcome to the Contact Manager System
+    </h1>
+</div>
+    </main>
+    <?php 
+    include ("footer.php"); 
+    ?>
+</body>
+</html>
